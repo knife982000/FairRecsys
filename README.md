e2b7c41c3d38807c0a4c1e90e3a2f4538a0c2919
Mikkel Andersen <miande22@student.aau.dk>
2025-03-02 19:07:46 +0100

    Base implementation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # P6
+
+Using [RecBole](https://github.com/RUCAIBox/RecBole)
+
+## Environment
+
+### [Python 3.8](https://www.python.org/downloads/release/python-380/)
+
+### Install packages
+
+```bash
+pip install -r .\requirements.txt
+```
+
+### Install Recbole
+
+```bash
+cd RecBole | pip install -e . --verbose
+```
---
+++ main.py
@@ -0,0 +1,87 @@
+import json
+import os
+from logging import getLogger
+from typing import Optional, Any, Dict
+
+from RecBole.recbole.quick_start.quick_start import load_data_and_model, run_recbole
+from RecBole.recbole.utils import get_trainer, set_color
+
+##################################
+######### Configurations #########
+##################################
+model_folder = "./saved_models/"
+metrics_results_folder = "./metrics_results/"
+
+methods = ["BPR", "LightGCN", "NGCF", "MultiVAE", "NNCF"]
+datasets = ["ml-100k"]
+config_dict = {
+    "metrics": ["Recall", "MRR", "NDCG", "Precision", "Hit", "Exposure"]
+}
+
+
+def is_model_trained(model: str) -> Optional[str]:
+    """
+    Check if the model has been trained on the specified dataset
+    :param model: ``str`` The name of the model
+    :return: ``str`` | ``None`` Name of the saved model if it exists
+    """
+    if os.path.isdir(config_dict["checkpoint_dir"]):
+        saved_models = os.listdir(config_dict["checkpoint_dir"])
+        for saved_model in saved_models:
+            if saved_model.find(model) != -1:
+                return saved_model
+    return None
+
+
+def run_and_evaluate_model(model: str, dataset: str) -> Dict[str, Any]:
+    """
+    Run and evaluate the model on the specified dataset
+    :param model: ``str`` The name of the model
+    :param dataset: ``str`` The name of the dataset
+    :return: ``Dict[str, Any]`` The evaluation results
+    """
+    config_dict["checkpoint_dir"] = model_folder + dataset
+    trained_model = is_model_trained(model)
+    if trained_model is None:
+        return run_recbole(model=model, dataset=dataset, config_dict=config_dict)
+
+    print(f"Model {model} has been trained on dataset {dataset}. Skipping training.")
+    return evaluate_pre_trained_model(model_folder + dataset + "/" + trained_model)
+
+
+def evaluate_pre_trained_model(model_path: str) -> Dict[str, Any]:
+    """
+    Evaluate a pre-trained model
+    :param model_path: ``str`` The path to the pre-trained model
+    :return: ``Dict[str, Any]`` The evaluation results
+    """
+    config, model, dataset, train_data, valid_data, test_data = load_data_and_model(model_path)
+
+    trainer = get_trainer(config["MODEL_TYPE"], config["model"])(config, model)
+    test_result = trainer.evaluate(test_data, load_best_model=False, show_progress=config["show_progress"])
+
+    logger = getLogger()
+    logger.info(set_color("test result", "yellow") + f": {test_result}")
+
+    return {"test_result": test_result}
+
+
+def save_metrics_results(model: str, dataset: str, results: Dict[str, Any]) -> None:
+    """
+    Save the evaluation results
+    :param model: ``str`` The name of the model
+    :param dataset: ``str`` The name of the dataset
+    :param results: ``Dict[str,Any]`` The evaluation results
+    :return: ``None``
+    """
+    path = f"{metrics_results_folder}{dataset}/"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(f"{path}{model}.json", "w") as file:
+        json.dump(results, file)
+
+
+if __name__ == "__main__":
+    for method in methods:
+        for dataset in datasets:
+            results = run_and_evaluate_model(method, dataset)
+            save_metrics_results(method, dataset, results)
