48b270825e90450ab7f68eddb19aaee553216b1b
Mikkel Andersen <miande22@student.aau.dk>
2025-03-04 11:44:45 +0100

    Removed boilerplate for metric, as it tried to load it

--- Recbole-Debias/recbole_debias/evaluator/metrics.py
+++ Recbole-Debias/recbole_debias/evaluator/metrics.py
@@ -1,16 +1,3 @@
 import numpy as np
 from recbole.evaluator import AbstractMetric, DataStruct
 from recbole.utils import EvaluatorType
-
-
-class CustomMetric(AbstractMetric):
-    """
-    TODO: Please follow https://recbole.io/docs/developer_guide/customize_metrics.html
-    to create customized metrics.
-    """
-
-    def __init__(self, config):
-        super().__init__(config)
-
-    def calculate_metric(self, dataobject):
-        pass
