c3f61331db779967aad11d1f59db53b2def70285
Mikkel Andersen <miande22@student.aau.dk>
2025-03-11 14:53:14 +0100

    Added novelty metric

--- RecBole/recbole/evaluator/metrics.py
+++ RecBole/recbole/evaluator/metrics.py
@@ -830,3 +830,32 @@ class Exposure(AbstractMetric):
             "Exposure_Disparity": round(self.exposure_disparity_popularity(exposure, items), self.decimal_place)
         }
         return results
+
+
+class Novelty(AbstractMetric):
+    metric_type = EvaluatorType.RANKING
+    metric_need = ["rec.items", "data.count_items"]
+
+    def __init__(self, config):
+        super().__init__(config)
+
+    def used_info(self, dataobject):
+        """Get the matrix of recommendation items and the popularity of items in training data"""
+        item_counter = dataobject.get("data.count_items")
+        item_matrix = dataobject.get("rec.items")
+        return item_matrix.numpy(), dict(item_counter)
+
+    def calculate_metric(self, dataobject):
+        item_matrix, item_count = self.used_info(dataobject)
+        result = self.novelty_score(item_matrix, item_count)
+        metric_dict = {"novelty": round(result, self.decimal_place)}
+        return metric_dict
+
+    def novelty_score(self, item_matrix, item_count):
+        novelty_scores = np.zeros_like(item_matrix, dtype=np.float)
+        for i in range(item_matrix.shape[0]):
+            for j in range(item_matrix.shape[1]):
+                item = item_matrix[i, j]
+                p_i = item_count.get(item, 1e-10)  # Avoid log(0) by using a small default value
+                novelty_scores[i, j] = -np.log2(p_i)
+        return novelty_scores.mean()
