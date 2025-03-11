f115a06a56eb3fc96bb99f64a21ab1543ce475c0
Mikkel Andersen <miande22@student.aau.dk>
2025-03-11 14:58:36 +0100

    Updated to show all gpus

--- gpu_test.py
+++ gpu_test.py
@@ -2,7 +2,8 @@ import torch
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
-        print("CUDA is available")
-        print(f"Device name: {torch.cuda.get_device_name(0)}")
+        print("CUDA is available, total devices: ", torch.cuda.device_count())
+        for i in range(torch.cuda.device_count()):
+            print(f"Device {i} name: {torch.cuda.get_device_name(i)}")
     else:
         print("CUDA is not available")
