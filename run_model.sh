33adfc7da30f912ca0d49822f47c5b07dbb633b3
Mikkel Andersen <miande22@student.aau.dk>
2025-03-14 15:03:45 +0100

    Update run_model.sh for better naming

--- run_model.sh
+++ run_model.sh
@@ -2,7 +2,8 @@
 #SBATCH --gres=gpu:4
 #SBATCH --mem=128g
 #SBATCH --cpus-per-task=15
-#SBATCH --job-name=Recbole-$2-$1
-#SBATCH --output=$1-$2-out.log
+#SBATCH --job-name=Recbole-${2}-${1}
+#SBATCH --output=%x-out.log
 
+echo "Running job for dataset: $1, model: $2"
 singularity exec --nv recbole.sif python3 main.py -d "$1" -m "$2"
