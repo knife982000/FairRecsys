e8c7873c24fc6dd6690631ac3cede5ab3a081f73
Mikkel Andersen <miande22@student.aau.dk>
2025-03-25 12:39:18 +0100

    Moved eval to separate file

--- single_evaluation.sh
+++ single_evaluation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DATASET="$1"
 MODEL="$2"
-GPUS="${3:-4}"
-MEMORY="$((GPUS * 32))G"
+GPUS="1"
+MEMORY="32G"
 JOB_NAME="Rec-${MODEL}-${DATASET}"
 OUTPUT_FILE="${DATASET}-${MODEL}.log"
-CPUS="$((GPUS * 10))"
-NODE="${4:-}"
+CPUS="10"
+NODE="${3:-}"
 
 echo "Submitting job with the following parameters:"
 echo "  Dataset:        $DATASET"
@@ -20,15 +20,15 @@ if [ -n "$NODE" ]; then
   echo "  Node:           $NODE"
 fi
 
-sbatch_command="sbatch --job-name=\"$JOB_NAME-Training\" \
-       --output=\"$OUTPUT_FILE-Training\" \
+sbatch_command_eval="sbatch --job-name=\"$JOB_NAME-Eval\" \
+       --output=\"$OUTPUT_FILE-Eval\" \
        --gres=gpu:$GPUS \
        --mem=$MEMORY \
        --cpus-per-task=$CPUS"
 
 if [ -n "$NODE" ]; then
-  sbatch_command="$sbatch_command --nodelist=\"$NODE\""
+  sbatch_command_eval="$sbatch_command_eval --nodelist=\"$NODE\""
 fi
 
-sbatch_command="$sbatch_command --wrap=\"singularity exec --nv recbole.sif python3 main.py -d \"$DATASET\" -m \"$MODEL\"\""
-eval $sbatch_command
+sbatch_command_eval="$sbatch_command_eval --wrap=\"singularity exec --nv recbole.sif python3 main.py -e True -d \"$DATASET\" -m \"$MODEL\"\""
+eval $sbatch_command_eval
