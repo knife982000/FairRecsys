3677f6faac21bfb837435a667d3ae74f848a69c0
Mikkel Andersen <miande22@student.aau.dk>
2025-03-25 12:30:29 +0100

    Added eval step to training

--- run_model.sh
+++ run_model.sh
@@ -4,7 +4,7 @@ MODEL="$2"
 GPUS="${3:-4}"
 MEMORY="$((GPUS * 32))G"
 JOB_NAME="Rec-${MODEL}-${DATASET}"
-OUTPUT_FILE="${DATASET}-${MODEL}-out.log"
+OUTPUT_FILE="${DATASET}-${MODEL}.log"
 CPUS="15"
 NODE="${4:-}"
 
@@ -20,7 +20,7 @@ if [ -n "$NODE" ]; then
   echo "  Node:           $NODE"
 fi
 
-sbatch_command="sbatch --job-name=\"$JOB_NAME\" \
+sbatch_command="sbatch --job-name=\"$JOB_NAME-Training\" \
        --output=\"$OUTPUT_FILE\" \
        --gres=gpu:$GPUS \
        --mem=$MEMORY \
@@ -31,5 +31,17 @@ if [ -n "$NODE" ]; then
 fi
 
 sbatch_command="$sbatch_command --wrap=\"singularity exec --nv recbole.sif python3 main.py -d \"$DATASET\" -m \"$MODEL\"\""
-
 eval $sbatch_command
+
+sbatch_command_eval="sbatch --job-name=\"$JOB_NAME-Eval\" \
+       --output=\"$OUTPUT_FILE-Eval\" \
+       --gres=gpu:1 \
+       --mem=32 \
+       --cpus-per-task=10"
+
+if [ -n "$NODE" ]; then
+  sbatch_command_eval="$sbatch_command_eval --nodelist=\"$NODE\""
+fi
+
+sbatch_command_eval="$sbatch_command_eval --wrap=\"singularity exec --nv recbole.sif python3 main.py -e True -d \"$DATASET\" -m \"$MODEL\"\""
+eval $sbatch_command_eval
