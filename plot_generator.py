2f54ffac61fe1d8307c7ecd66d4a5518cdf3a2db
Mikkel Andersen <miande22@student.aau.dk>
2025-04-01 10:55:53 +0200

    Changed to use plotly as matplotlib cannot handle the amount of data

--- plot_generator.py
+++ plot_generator.py
@@ -1,33 +1,69 @@
 import json
 import os
 from typing import Dict, List, Any
-
-import matplotlib.pyplot as plt
-
+import plotly.graph_objects as go
+import numpy as np
 results_path = "./metrics_results/results.json"
 methods =  ["BPR", "LightGCN", "NGCF", "MultiVAE", "Random"]
 datasets = ["ml-100k", "ml-1m", "ml-20m", "gowalla-merged", "steam-merged"]
 datasets_formatted = ["ML-100K", "ML-1M", "ML-20M", "Gowalla", "Steam"]
 
 
+# def plot_data(exposure_data: Dict[str, List[float]], dataset: str, fair_exposure: float):
+#     fig, ax = plt.subplots(figsize=(6, 4))
+#     for method, data in exposure_data.items():
+#         exposure = sorted(data, reverse=True)
+#         items = [i for i in range(len(exposure))]
+#         ax.scatter(items, exposure, s=1, alpha=0.6, label=method)
+#     ax.axhline(y=fair_exposure, color='black', linestyle='--', label='Fair Exposure')
+#     ax.set_xlabel('Items')
+#     ax.set_ylabel('Exposure')
+#     ax.legend()
+#     folder = "./plots/"
+#     os.makedirs(os.path.dirname(folder), exist_ok=True)
+#     plt.savefig(f"{folder}{dataset}.png", dpi=300)
+#     plt.close()
+
+
 def plot_data(exposure_data: Dict[str, List[float]], dataset: str, fair_exposure: float):
-    plt.figure(figsize=(6, 4))
+    fig = go.Figure()
+    highest_exposure = 0
     for method, data in exposure_data.items():
-        exposure = sorted(data, reverse=True)
-        items = [i for i in range(len(exposure))]
-        plt.scatter(items, exposure, s=3, alpha=0.6, label=method)
-    plt.axhline(y=fair_exposure, color='black', linestyle='--', label='Fair Exposure')
-    plt.xlabel('Items')
-    plt.ylabel('Exposure')
-    plt.suptitle('Exposure of Each Recommended Items')
-    plt.title(f"{datasets_formatted[datasets.index(dataset)]}")
-    plt.legend()
+        exposure = np.asarray([e if e > 0 else 1e-1 for e in sorted(data, reverse=True)])
+        highest_exposure = max(highest_exposure, exposure[0])
+        items = np.asarray(list(range(len(exposure))))
+        fig.add_trace(go.Scattergl(x=items, y=exposure, mode='markers', name=method))
+    fig.add_trace(go.Scatter(x=[0, len(items)], y=[fair_exposure, fair_exposure], mode='lines', name='Fair Exposure', line=dict(dash='dash')))
+    y_max = 0
+    for i in range(1, 100):
+        if (10 ** (i + 0.5)) > highest_exposure:
+            y_max = i + 0.5
+            break
+    fig.update_layout(
+        xaxis_title='Items',
+        yaxis_title='Exposure',
+        legend_title='',
+        xaxis_type='log',
+        yaxis_type='log',
+        yaxis_exponentformat='none',
+        yaxis_range=[-1.1, y_max],
+        yaxis_tickvals=[0.1, 1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 20000, 50000, 100000],
+        yaxis_ticktext=["0", "1", "5", "10", "50", "100", "500", "1K", "5K", "10K", "20K", "50K", "100K"],
+        xaxis_tickvals=[0.1, 1, 5, 10, 100, 1000, 10000, 100000, 1000000],
+        xaxis_ticktext=["0", "1", "5", "10", "100", "1K", "10K", "100K", "1M"],
+        font=dict(size=18),
+        margin=dict(l=0, r=0, t=2, b=2),
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=-0.25,
+            xanchor="center",
+            x=0.5
+        )
+    )
     folder = "./plots/"
     os.makedirs(os.path.dirname(folder), exist_ok=True)
-    plt.savefig(f"{folder}{dataset}.png", dpi=300)
-    plt.show()
-    plt.close()
-
+    fig.write_image(f"{folder}{dataset}.png")
 
 def load_data(path: str):
     if os.path.exists(path):
@@ -61,7 +97,7 @@ def fill_table(dataset: str, k: int, results: Dict[str, Any]) -> str:
         is_exposure = "exposure" in metric
         table_data[metric] = bold_max(table_data[metric], is_exposure)
 
-    table_str = f"\\begin{{table*}}\n \\caption{{Metrics results for the {dataset} dataset, where the k value is {k}.}}\n  \\label{{table:results_{dataset}}}\n  \\centering\n  \\begin{{tabular}}{{lcccccccccc}}\n    \\toprule\n    \\textbf{{Model}} & \\textbf{{Recall}} & \\textbf{{MRR}} & \\textbf{{NDCG}} & \\textbf{{Hit}} & \\textbf{{SE}} & \\textbf{{Novelty}} & \\textbf{{Exp 50-50}} & \\textbf{{Exp 80-20}} & \\textbf{{Exp 90-10}} & \\textbf{{Exp 99-1}} \\\\\n    \\midrule\n"
+    table_str = f"\\begin{{table*}}\n \\caption{{Metrics results for the {dataset} dataset, where the k value is {k}.}}\n  \\label{{table:results_{dataset}}}\n  \\centering\n  \\begin{{tabular}}{{lcccccccccc}}\n    \\toprule\n    \\textbf{{Model}} & \\textbf{{Recall}} & \\textbf{{MRR}} & \\textbf{{NDCG}} & \\textbf{{Hit}} & \\textbf{{SE}} & \\textbf{{Novelty}} & \\textbf{{DE$_{{50-50}}$}} & \\textbf{{DE$_{{80-20}}$}} & \\textbf{{DE$_{{90-10}}$}} & \\textbf{{DE$_{{99-1}}$}} \\\\\n    \\midrule\n"
     for i, model in enumerate(models):
         table_str += f"    {model} & {table_data['recall@10'][i]} & {table_data['mrr@10'][i]} & {table_data['ndcg@10'][i]} & {table_data['hit@10'][i]} & {table_data['shannonentropy@10'][i]} & {table_data['novelty'][i]} & {table_data['exposure_50-50@10'][i]} & {table_data['exposure_80-19@10'][i]} & {table_data['exposure_90-9@10'][i]} & {table_data['exposure_99-1@10'][i]} \\\\\n"
     table_str += "    \\bottomrule\n  \\end{tabular}\n\\end{table*}"
@@ -87,6 +123,8 @@ if __name__ == "__main__":
             method_data = {}
             fair_exposure = 0
             for method in methods:
+                if method not in dataset_results:
+                    continue
                 fair_exposure = dataset_results[method]["fair_exposure"]
-                method_data[method] = dataset_results[method]["plot_data"] + [0] * (dataset_results[method]["num_items"] - len(dataset_results[method]["plot_data"]))
+                method_data[method] = dataset_results[method]["plot_data"] + [-0.1] * (dataset_results[method]["num_items"] - len(dataset_results[method]["plot_data"]))
             plot_data(method_data, dataset, fair_exposure)
