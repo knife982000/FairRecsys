077ddad05515979d175af2ea26e71a1ca5d271a3
Mikkel Andersen <miande22@student.aau.dk>
2025-04-15 13:31:29 +0200

    Removed print statements

--- RecboleRunner/runner_manager.py
+++ RecboleRunner/runner_manager.py
@@ -7,9 +7,7 @@ class RunnerManager:
     @classmethod
     def set_runner(cls, runner: RecboleRunner):
         cls._runner = runner
-        print(f"RecboleRunner set with model: {runner.model_name}, dataset: {runner.dataset_name}")
 
     @classmethod
     def get_runner(cls) -> RecboleRunner:
-        print("RecboleRunner retrieved: ", cls._runner)
         return cls._runner
