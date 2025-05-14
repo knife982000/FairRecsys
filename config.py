bd7cf21206311455f6b6e0d4c0e81a3fb631e8d4
Mikkel Andersen <miande22@student.aau.dk>
2025-05-14 11:04:20 +0200

    Added tailpercentage

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ methods = {"BPR": None, "LightGCN": None, "NGCF": None,"Random": None, "BPREntro
 
 datasets = ["ml-100k", "ml-1m", "ml-20m", "gowalla-merged", "steam-merged"]
 config_dictionary = {
-    "metrics": ["Recall", "MRR", "NDCG", "Precision", "Hit", "Exposure", "ShannonEntropy", "Novelty", "RecommendedGraph"]
+    "metrics": ["Recall", "MRR", "NDCG", "Precision", "Hit", "Exposure", "ShannonEntropy", "Novelty", "RecommendedGraph", "TailPercentage"]
 }
 config_file = ["config.yaml"]
 eval_config_file = ["eval_config.yaml"]
